d26af160b98947f8dad6c42ed5da180c59e3bdc9
Erin Wise <erinwise88@gmail.com>
2024-01-11 23:37:50 -0500

    Work on helper functions and cli.py, debugging

--- lib/cli.py
+++ lib/cli.py
@@ -1,5 +1,11 @@
 # lib/cli.py
 
+##HELP
+#1) Checking for string not working b/c input of 0 or '0' is the same...
+#2) Update - getting the right number - I want user to be able to enter the number then do find_by_id
+#3) How do I make it show lower down and in nicer format (i.e. Teams > 5)
+
+
 ##Choose from the Following
 
 ##Show all Teams - 'T' tp see Teams
@@ -30,11 +36,19 @@
 from helpers import (
     exit_program,
     list_teams,
-    list_players
-    # helper_1
-)
+    list_players,
+    add_team,
+    view_team,
+    update_team,
+    delete_team,
+    view_roster,
+    view_player,
+    add_player,
+    update_player,
+    delete_player
+    )
 
-import click
+# import click
 
 def main():
     while True:
@@ -44,19 +58,87 @@ def main():
         if choice == "0":
             exit_program()
         elif choice == "1":
-            list_teams()
+            teams()
         elif choice == "2":
-            list_players()
+            players()
         else:
             print("Invalid choice")
 
 
 def menu():
+    print("-----------Junior Hockey Organization----------")
     print("Please select an option:")
     print("0. Exit the program")
-    print("1. List all Teams")
-    print("2. List all Players")
+    print("1. Teams")
+    print("2. Players")
 
+def teams():
+    while True:
+        
+        teams_menu()
+        teams_choice = input(">")
+        if teams_choice == "0":
+            exit_program()
+            break
+        elif teams_choice == "1":
+            view_team()
+        elif teams_choice == "2":
+            add_team()
+        elif teams_choice == "3":
+            update_team()
+        elif teams_choice == "4":
+            delete_team()
+        elif teams_choice == "5":
+            view_roster()
+        elif team_choice == "6":
+            main()
+        else:
+            print("Invalid choice")
+    
+def teams_menu():
+    print("-----------TEAMS----------")
+    list_teams()
+    print("-----------SELECT AN OPTION BELOW----------")
+    print("0. Exit the program")
+    print("1. View Team Details")
+    print("2. Add New Team")
+    print("3. Update a Team")
+    print("4. Delete a Team")
+    print("5: View Team Roster")
+    print("6: Return to Previous Menu")
+    
+
+def players():
+    while True:
+        
+        players_menu()
+        players_choice = input(">")
+        if players_choice == "0":
+            exit_program()
+            break
+        elif players_choice == "1":
+            view_player()
+        elif players_choice == "2":
+            add_player()
+        elif players_choice == "3":
+            update_player()
+        elif players_choice == "4":
+            delete_player()
+        elif players_choice == "5":
+            main()
+        else:
+            print("Invalid choice")
+
+def players_menu():
+    print("-----------PLAYERS----------")
+    print("-----------SELECT AN OPTION BELOW----------")
+    print("0. Exit the program")
+    print("1. View Player Details")
+    print("2. Add New Player")
+    print("3. Update a Player")
+    print("4. Delete a Player")
+    ## Search for Players By: Option 1: Age, Option2: Position
+    print("5: Return to Previous Menu")
 
 if __name__ == "__main__":
     main()
--- lib/helpers.py
+++ lib/helpers.py
@@ -2,20 +2,98 @@
 from models.player import Player
 from models.team import Team
 
-# def helper_1():
-#     print("Performing useful function#1.")
 
 
 def exit_program():
     print("Goodbye!")
     exit()
 
+#TEAM HELPERS
 def list_teams():
     teams = Team.get_all()
-    for team in teams:
-        print(team)
+    for i, team in enumerate(teams, start = 1):
+        print(f"{i}. {team.name}")
 
 def list_players():
     players = Player.get_all()
-    for player in players:
-        print(player)
+    for i, player in enumerate(players, start = 1):
+        print(f"{i}. {player.name}")
+
+def add_team():
+    name = input("Enter Team Name: ")
+    color = input("Enter Team Color: ")
+    mascot = input("Enter Team Mascot: ")
+    try:
+        new_team = Team.create(name, color, mascot)
+        print(f"Success! {new_team.name} Added!")
+    except Exception as exc:
+        print("Error creating team: ", exc)
+
+def view_team():
+    list_teams()
+    name = input("Enter Team Name For Details: ")
+    try:
+        team_to_view = Team.find_by_name(name)
+        print(f"Team Name: {team_to_view.name}, Team Color: {team_to_view.color}, Mascot: {team_to_view.mascot}")
+    except Exception as exc:
+        print('Team Not Found', exc)
+
+def update_team():
+    list_teams()
+    # id_ = input("Enter the number for the team: ")    
+    # # print(id_) = 1
+    
+    # if team := Team.find_by_id((int(id_)) - 1):
+    name = input("Enter the Team Name to Update: ")
+    if team := Team.find_by_name(name):
+        try:
+            print(team)
+            name = input("New Team Name: ")
+            team.name = name
+            color = input("New Team Color: ")
+            team.color = color
+            mascot = input("New Team Mascot: ")
+            team.mascot = mascot
+            team.update()
+            print(f'Success! {team.name} had been updated!')
+            print(f'"Team Name: {team.name}, Team Color: {team.color}, Mascot: {team.mascot}')
+        except Exception as exc:
+            print(f'Error updating Team', exc)
+    else:
+        print(f'Team {name} Not Found')
+
+
+def delete_team():
+    list_teams()
+    name = input("Enter the Team Name: ")
+    if team := Team.find_by_name(name):
+        team.delete()
+        print(f'Team: {team.name} has been deleted.')
+    else:
+        print(f"Team: {team.name} not found!")
+
+def view_roster():
+    list_teams()
+    name = input("Select Team to View Roster: ")
+
+    if team := Team.find_by_name(name):
+        roster = team.players()
+        print(f'{team.name} Roster:')
+        print("\n")
+        for i in roster:
+            print(f"Name: {i.name}, Age: {i.age}, Position: {i.position}")
+    
+
+##PLAYER HELPERS
+
+def view_player():
+    pass
+
+def add_player():
+    pass
+
+def update_player():
+    pass
+
+def delete_player():
+    pass
